7bc9196ed5b5cd58a7b6800bd2be460329428359
Jose Enmanuel Jaquez <jose_jaquez1@ucne.edu.do>
2020-08-25 23:25:28 -0400

    Add examples files VHDL

---
+++ Hola mundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hola_mundo
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo, Jose Enmanuel de este lado");
+
+            Console.WriteLine("\nCual es tu nombre?");
+            var nombre = Console.ReadLine();
+            var datos = DateTime.Now;
+
+            Console.WriteLine($"\n Hola,{nombre},y {datos:A} y {datos: r}!");
+            Console.WriteLine("Precione una tecla para terminar");
+            Console.ReadKey(true);
+        }
+    }
+}
